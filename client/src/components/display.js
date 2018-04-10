00ad36b8436bf0f9cbf5743ccd996143f6c2c8ee
JieWang210071 <wangjie940317@gmail.com>
2018-04-09 23:28:56 -0700

    add client side

---
+++ client/src/components/display.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Login extends Component {
+    render() {
+        return (
+          <div className="App">
+            No movies here, please login again.
+          </div>
+        );
+      }
+}
+
+export default Login;
